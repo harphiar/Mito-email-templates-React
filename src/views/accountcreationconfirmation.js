26c5734481ca9578578895dea2dca6a6718a7b14
harphiar <iyanu.ademade@gmail.com>
2025-05-13 14:26:05 +0100

    Commit from TeleportHQ

---
+++ src/views/accountcreationconfirmation.js
@@ -0,0 +1,259 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './accountcreationconfirmation.css'
+
+const Accountcreationconfirmation = (props) => {
+  return (
+    <div className="accountcreationconfirmation-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="accountcreationconfirmation-accountcreationconfirmation">
+        <div className="accountcreationconfirmation-text10">
+          <span className="accountcreationconfirmation-text11">
+            Welcome! Your new account is all set up.
+          </span>
+        </div>
+        <div className="accountcreationconfirmation-frame1867">
+          <div className="accountcreationconfirmation-frame18541">
+            <div className="accountcreationconfirmation-text12">
+              <span className="accountcreationconfirmation-text13">
+                Hello John
+              </span>
+            </div>
+            <div className="accountcreationconfirmation-text14">
+              <span className="accountcreationconfirmation-text15">
+                Great news! Your account has been successfully created by our
+                administrative team and is now ready for you to access. Below
+                are all the details you&apos;ll need to get started.
+              </span>
+            </div>
+          </div>
+          <div className="accountcreationconfirmation-frame2833">
+            <div className="accountcreationconfirmation-text16">
+              <span className="accountcreationconfirmation-text17 Paragraphbold">
+                Your account details
+              </span>
+            </div>
+            <div className="accountcreationconfirmation-frame2832">
+              <div className="accountcreationconfirmation-frame2830">
+                <div className="accountcreationconfirmation-text18">
+                  <span className="accountcreationconfirmation-text19 PSmall">
+                    Username
+                  </span>
+                </div>
+                <div className="accountcreationconfirmation-text20">
+                  <span className="accountcreationconfirmation-text21 PSmall">
+                    Temporary password
+                  </span>
+                </div>
+                <div className="accountcreationconfirmation-text22">
+                  <span className="accountcreationconfirmation-text23 PSmall">
+                    Account type
+                  </span>
+                </div>
+                <div className="accountcreationconfirmation-text24">
+                  <span className="accountcreationconfirmation-text25 PSmall">
+                    Account ID
+                  </span>
+                </div>
+                <div className="accountcreationconfirmation-text26">
+                  <span className="accountcreationconfirmation-text27 PSmall">
+                    Creation date
+                  </span>
+                </div>
+              </div>
+              <div className="accountcreationconfirmation-frame2831">
+                <div className="accountcreationconfirmation-text28">
+                  <span className="accountcreationconfirmation-text29 PSmall">
+                    jameselliotdoe
+                  </span>
+                </div>
+                <div className="accountcreationconfirmation-text30">
+                  <span className="accountcreationconfirmation-text31 PSmall">
+                    Tmp12345Lq
+                  </span>
+                </div>
+                <div className="accountcreationconfirmation-text32">
+                  <span className="accountcreationconfirmation-text33 PSmall">
+                    User
+                  </span>
+                </div>
+                <div className="accountcreationconfirmation-text34">
+                  <span className="accountcreationconfirmation-text35 PSmall">
+                    #0123456
+                  </span>
+                </div>
+                <div className="accountcreationconfirmation-text36">
+                  <span className="accountcreationconfirmation-text37 PSmall">
+                    22 Apr, 2023
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="accountcreationconfirmation-frame2834">
+            <div className="accountcreationconfirmation-text38">
+              <span className="accountcreationconfirmation-text39 Paragraphbold">
+                Important first steps
+              </span>
+            </div>
+            <div className="accountcreationconfirmation-text40">
+              <span className="accountcreationconfirmation-text41">
+                <span className="accountcreationconfirmation-text42">
+                  Log in immediately at mito.money
+                </span>
+                <span className="accountcreationconfirmation-text43">
+                  /login
+                </span>
+                <br></br>
+                <span>
+                  Change your temporary password - You&apos;ll be prompted to do
+                  this on your first login
+                </span>
+                <br></br>
+                <span>
+                  Complete your profile - Add your contact information and
+                  preferences
+                </span>
+                <br></br>
+                <span>
+                  Set up two-factor authentication - For additional security
+                  (highly recommended)
+                </span>
+              </span>
+            </div>
+          </div>
+          <div className="accountcreationconfirmation-frame18542">
+            <div className="accountcreationconfirmation-text50">
+              <span className="accountcreationconfirmation-text51 Paragraph">
+                <span>
+                  If you have any questions or require further clarification,
+                  kindly contact us at your earliest convenience.
+                </span>
+                <br></br>
+                <span>Thank you, best regards</span>
+                <br></br>
+                <span>
+                  Mito team
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+            </div>
+          </div>
+          <div className="accountcreationconfirmation-frame1865">
+            <div className="accountcreationconfirmation-frame1863">
+              <img
+                alt="mail1437"
+                src="/external/mail1437-i7ei.svg"
+                className="accountcreationconfirmation-mail"
+              />
+              <div className="accountcreationconfirmation-text57">
+                <span className="accountcreationconfirmation-text58 PSmall">
+                  info@mito.money
+                </span>
+              </div>
+            </div>
+            <div className="accountcreationconfirmation-frame1864">
+              <img
+                alt="globe1437"
+                src="/external/globe1437-jjxn.svg"
+                className="accountcreationconfirmation-globe"
+              />
+              <div className="accountcreationconfirmation-text59">
+                <span className="accountcreationconfirmation-text60 PSmall">
+                  https://www.mito.money
+                </span>
+              </div>
+            </div>
+            <div className="accountcreationconfirmation-text61">
+              <span className="accountcreationconfirmation-text62 PSmall">
+                <span>Rhemito is a trademark owned by</span>
+                <br></br>
+                <span>
+                  Funtech Global Communications Ltd. Devonshire House, Manor
+                  way, Borehamwood, Herts. WD6 1QQ, United Kingdom
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="accountcreationconfirmation-emailheader">
+          <div className="accountcreationconfirmation-section-hero">
+            <img
+              alt="Ellipse181861"
+              src="/external/ellipse181861-wbi-200h.png"
+              className="accountcreationconfirmation-ellipse18"
+            />
+            <img
+              alt="Ellipse201861"
+              src="/external/ellipse201861-o1e-200h.png"
+              className="accountcreationconfirmation-ellipse20"
+            />
+            <img
+              alt="Rectangle181861"
+              src="/external/rectangle181861-t3o-200h.png"
+              className="accountcreationconfirmation-rectangle18"
+            />
+          </div>
+          <div className="accountcreationconfirmation-logolockuph">
+            <div className="accountcreationconfirmation-layer2">
+              <div className="accountcreationconfirmation-logoicon">
+                <div className="accountcreationconfirmation-layer2copy">
+                  <img
+                    alt="VectorI186"
+                    src="/external/vectori186-xy1i.svg"
+                    className="accountcreationconfirmation-vector1"
+                  />
+                  <img
+                    alt="VectorI186"
+                    src="/external/vectori186-0k58.svg"
+                    className="accountcreationconfirmation-vector2"
+                  />
+                  <img
+                    alt="VectorI186"
+                    src="/external/vectori186-2i71r.svg"
+                    className="accountcreationconfirmation-vector3"
+                  />
+                  <img
+                    alt="VectorI186"
+                    src="/external/vectori186-haap.svg"
+                    className="accountcreationconfirmation-vector4"
+                  />
+                  <img
+                    alt="VectorI186"
+                    src="/external/vectori186-dac6.svg"
+                    className="accountcreationconfirmation-vector5"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            alt="Ellipse21861"
+            src="/external/ellipse21861-6uho-200h.png"
+            className="accountcreationconfirmation-ellipse2"
+          />
+          <img
+            alt="Ellipse31861"
+            src="/external/ellipse31861-jdkk-200h.png"
+            className="accountcreationconfirmation-ellipse3"
+          />
+          <img
+            alt="mailreceived11861"
+            src="/external/mailreceived11861-0yk-200w.png"
+            className="accountcreationconfirmation-mailreceived1"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Accountcreationconfirmation
